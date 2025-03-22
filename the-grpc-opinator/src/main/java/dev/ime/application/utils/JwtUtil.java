621004c3ab04ceddee8cc9aa703814bcdcdc8d56
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2025-03-22 07:34:30 +0100

    refactor: move dependency

--- the-grpc-opinator/src/main/java/dev/ime/application/utils/JwtUtil.java
+++ the-grpc-opinator/src/main/java/dev/ime/application/utils/JwtUtil.java
@@ -6,7 +6,7 @@ import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Component;
 
-import dev.ime.common.config.GlobalConstants;
+import dev.ime.common.constants.GlobalConstants;
 
 @Component
 public class JwtUtil {
